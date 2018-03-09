31bbc366ba633fb405a2c1f74a070638cc4cd06e
VladTemian <temian.vlad@gmail.com>
2018-03-09 18:54:12 +0200

    updatd navbar styling

--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
-s3cmd del s3://demo.frontted.com/vision/090320181540/* &&
-s3cmd del s3://demo.frontted.com/vision/090320181540/**/**/* &&
-s3cmd sync -MP dist/ s3://demo.frontted.com/vision/090320181540/ &&
-s3cmd put -P --mime-type=text/css dist/assets/css/*.css s3://demo.frontted.com/vision/090320181540/assets/css/ &&
-s3cmd put -P --mime-type=text/css dist/assets/vendor/*.css s3://demo.frontted.com/vision/090320181540/assets/vendor/
+s3cmd del s3://demo.frontted.com/vision/090320181637/* &&
+s3cmd del s3://demo.frontted.com/vision/090320181637/**/**/* &&
+s3cmd sync -MP dist/ s3://demo.frontted.com/vision/090320181637/ &&
+s3cmd put -P --mime-type=text/css dist/assets/css/*.css s3://demo.frontted.com/vision/090320181637/assets/css/ &&
+s3cmd put -P --mime-type=text/css dist/assets/vendor/*.css s3://demo.frontted.com/vision/090320181637/assets/vendor/
