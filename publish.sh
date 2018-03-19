a78a4b8bb1da2fb4f4439a753b1034960bdcdd5f
VladTemian <temian.vlad@gmail.com>
2018-03-19 18:50:44 +0200

    fixed + cleanup sidebars

--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
-s3cmd del s3://demo.frontted.com/vision/160320181214/* &&
-s3cmd del s3://demo.frontted.com/vision/160320181214/**/**/* &&
-s3cmd sync -MP dist/ s3://demo.frontted.com/vision/160320181214/ &&
-s3cmd put -P --mime-type=text/css dist/assets/css/*.css s3://demo.frontted.com/vision/160320181214/assets/css/ &&
-s3cmd put -P --mime-type=text/css dist/assets/vendor/*.css s3://demo.frontted.com/vision/160320181214/assets/vendor/
+s3cmd del s3://demo.frontted.com/vision/160320181856/* &&
+s3cmd del s3://demo.frontted.com/vision/160320181856/**/**/* &&
+s3cmd sync -MP dist/ s3://demo.frontted.com/vision/160320181856/ &&
+s3cmd put -P --mime-type=text/css dist/assets/css/*.css s3://demo.frontted.com/vision/160320181856/assets/css/ &&
+s3cmd put -P --mime-type=text/css dist/assets/vendor/*.css s3://demo.frontted.com/vision/160320181856/assets/vendor/
