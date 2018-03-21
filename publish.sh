ca79fc01b12af51c72ef04fd80f338ea73d0e35c
VladTemian <temian.vlad@gmail.com>
2018-03-21 17:05:23 +0200

    fixed navbar fixed links for pages and ui components

--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
-s3cmd del s3://demo.frontted.com/vision/160320181856/* &&
-s3cmd del s3://demo.frontted.com/vision/160320181856/**/**/* &&
-s3cmd sync -MP dist/ s3://demo.frontted.com/vision/160320181856/ &&
-s3cmd put -P --mime-type=text/css dist/assets/css/*.css s3://demo.frontted.com/vision/160320181856/assets/css/ &&
-s3cmd put -P --mime-type=text/css dist/assets/vendor/*.css s3://demo.frontted.com/vision/160320181856/assets/vendor/
+s3cmd del s3://demo.frontted.com/vision/200320181418/* &&
+s3cmd del s3://demo.frontted.com/vision/200320181418/**/**/* &&
+s3cmd sync -MP dist/ s3://demo.frontted.com/vision/200320181418/ &&
+s3cmd put -P --mime-type=text/css dist/assets/css/*.css s3://demo.frontted.com/vision/200320181418/assets/css/ &&
+s3cmd put -P --mime-type=text/css dist/assets/vendor/*.css s3://demo.frontted.com/vision/200320181418/assets/vendor/
