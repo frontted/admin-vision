efa009c8dc421365faa42af6596a7cbeca9365e4
VladTemian <temian.vlad@gmail.com>
2018-03-22 19:43:12 +0200

    fixed publish sh

--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
-s3cmd del s3://demo.frontted.com/vi1941/220320181418/* &&
-s3cmd del s3://demo.frontted.com/vi1941/220320181418/**/**/* &&
-s3cmd sync -MP dist/ s3://demo.frontted.com/vi1941/220320181418/ &&
+s3cmd del s3://demo.frontted.com/vision/220320181941/* &&
+s3cmd del s3://demo.frontted.com/vision/220320181941/**/**/* &&
+s3cmd sync -MP dist/ s3://demo.frontted.com/vision/220320181941/ &&
 s3cmd put -P --mime-type=text/css dist/assets/css/*.css s3://demo.frontted.com/vision/220320181941/assets/css/ &&
 s3cmd put -P --mime-type=text/css dist/assets/vendor/*.css s3://demo.frontted.com/vision/220320181941/assets/vendor/
