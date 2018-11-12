985712e9b9c362b05b8a4641f3101469c03d76bb
Adrian Demian <adrian@themefy.io>
2018-11-13 00:46:51 +1100

    cleanup + update stuff

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,32 +1,13 @@
-Object.values = Object.values || (obj => Object.keys(obj).map(key => obj[key]));
-
 require('theme-mix')
 
 // Enable sourcemaps
 const { mix } = require('laravel-mix')
-
 const sourceMapsInProduction = false
-
 mix.sourceMaps(sourceMapsInProduction)
 
-if (mix.inProduction()) {
-  // FIX PRODUCTION JS COMPILATION
-  const merge   = require('webpack-merge').smart
-  let webpackConfig = {}
-
-  webpackConfig = merge(webpackConfig, {
-      module: {
-        rules: [{
-          test: /\.js?$/,
-          exclude: /node_modules(?!\/@material)|bower_components/,
-          use: [{
-              loader: 'babel-loader',
-              options: mix.config.babel()
-          }]
-        }]
-      }
-    })
-  webpackConfig = merge(Config.webpackConfig, webpackConfig)
-  mix.webpackConfig(webpackConfig)
+// https://github.com/JeffreyWay/laravel-mix/issues/1793
+if (!mix.inProduction()) {
+  mix.webpackConfig({
+    devtool: 'inline-source-map'
+  })
 }
-
